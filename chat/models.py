c50909c0e1afec43fffc04a60cef0c52a6ef7178
helis.inexture <helis.inexture@gmail.com>
2022-07-28 19:14:38 +0530

    update models.py

--- chat/models.py
+++ chat/models.py
@@ -1,20 +1,46 @@
-from django.contrib.auth.models import AnonymousUser
+from users.models import CustomUser
+
+from django.contrib.auth.models import User, AnonymousUser
 from django.db import models
 from django.db.models import SET
-
-from users.models import CustomUser
 
 
 class Room(models.Model):
     """
     model for chat rooms
     """
-    sender_user = models.ForeignKey(CustomUser, related_name='room_sender', on_delete=SET(AnonymousUser.id))
-    receiver_user = models.ForeignKey(CustomUser, related_name='room_receiver', on_delete=SET(AnonymousUser.id))
     room_name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.room_name
+
+
+class PersonalRoom(models.Model):
+    """
+    model for storing chat rooms for 2 users
+    """
+    room = models.ForeignKey(Room, related_name='chat_room', on_delete=models.CASCADE)
+    sender_user = models.ForeignKey(CustomUser, related_name='room_sender', on_delete=SET(AnonymousUser.id))
+    receiver_user = models.ForeignKey(CustomUser, related_name='room_receiver', on_delete=SET(AnonymousUser.id))
+
+
+class Group(models.Model):
+    """
+    models for storing group name with its corresponding room
+    """
+    room = models.ForeignKey(Room, related_name='room', on_delete=models.CASCADE)
+    group_name = models.CharField(max_length=25, blank=False, null=False)
+
+    def __str__(self):
+        return self.group_name
+
+
+class GroupMembers(models.Model):
+    """
+    model for storing group members
+    """
+    user = models.ForeignKey(CustomUser, related_name='group_member', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, related_name='group_room', on_delete=models.CASCADE)
 
 
 class Message(models.Model):
